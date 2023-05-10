65b29c0041545d277374a8d9d4dde55d5a84153c
orlovstepan <orlovst94@gmail.com>
2023-05-10 21:43:48 +0200

    playing around with vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,99 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Todo List</h1>
+    <ul>
+      <li v-bind:class="(todo.completed ? 'completed' : 'not-completed')" v-for="(todo, index) in todos" :key="index">
+        <TodoItem :title="todo.title" :completed="todo.completed" @update:completed="updateTodo(index, $event)" />
+      </li>
+    </ul>
+    <input class="add-todo" v-model="newTodo" @keyup.enter="addTodo" placeholder="Add a new todo" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoItem from '@/components/TodoItem.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    TodoItem,
+  },
+  data() {
+    return {
+      newTodo: '',
+      todos: [
+        { title: 'Buy groceries', completed: false },
+        { title: 'Finish project', completed: true },
+      ],
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim()) {
+        this.todos.push({ title: this.newTodo, completed: false });
+        this.newTodo = '';
+      }
+    },
+    updateTodo(index, completed) {
+      this.todos[index].completed = completed;
+    },
+  },
+};
 </script>
 
 <style>
+body {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  margin: 0;
+  padding: 0;
+  background-color: #f5f5f5;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  max-width: 500px;
+  margin: 50px auto;
+  padding: 20px;
+  background-color: #ffffff;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  border-radius: 5px;
+}
+
+h1 {
+  font-size: 24px;
+  font-weight: bold;
+  color: #333333;
+  margin-bottom: 20px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0 0 20px 0;
+}
+
+li {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 8px 12px;
+  background-color: #f8f9fa;
+  border: 1px solid #e9ecef;
+  border-radius: 4px;
+  margin-bottom: 10px;
+}
+
+.add-todo {
+  width: 100%;
+  padding: 8px 12px;
+  box-sizing: border-box;
+  border: 1px solid #cccccc;
+  border-radius: 4px;
+  font-size: 16px;
+}
+.completed {
+  text-decoration: line-through;
+  color: #adb5bd;
 }
 </style>
+
+
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="todo-item">
+    <input type="checkbox" :checked="completed" @change="toggleCompleted" />
+    <label>{{ title }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: ['title', 'completed'],
+  methods: {
+    toggleCompleted() {
+      this.$emit('update:completed', !this.completed);
+    },
+  },
+};
+</script>
+
+<style scoped>
+input[type='checkbox'] {
+  margin-right: 10px;
+}
+</style>
+
+
